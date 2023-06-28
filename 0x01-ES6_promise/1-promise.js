f75c79c9fd000090dfb5a9d814ddb62be23902a8
bollo-omar <bolloaggrey@gmail.com>
2023-06-28 12:10:39 +0000

    reveision task o1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -7,7 +7,7 @@ export default function getFullResponseFromAPI(success){
 			});
 		}
 		 else {
-		 	reject(new Error('The fake API is not working currently'));
+		 	reject(Error('The fake API is not working currently'));
 		 }
 	 });
 }
