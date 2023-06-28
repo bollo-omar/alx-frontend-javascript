1962ae30f96cef3258be92b907495e113c6c1b77
bollo-omar <bolloaggrey@gmail.com>
2023-06-28 12:15:39 +0000

    revision task o1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,14 @@
-export default function getFullResponseFromAPI(success){
-	 return new Promise(function(resolve, reject){
-		if(success){
-			resolve({
-				status: 200,
-				body : 'success'
-			});
-		}
-		 else {
-		 	reject(Error('The fake API is not working currently'));
-		 }
-	 });
+export default function (success) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (success === true) {
+      resolve(object);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
