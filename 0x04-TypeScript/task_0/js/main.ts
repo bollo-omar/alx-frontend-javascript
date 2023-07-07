a1e22ae8dcc575ab70548327105e702367dc333f
bollo18 <bolloo18@gmail.com>
2023-07-07 14:45:31 +0300

    finsied refactoring

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,48 +1,56 @@
+// Write an interface named Student that accepts the following elements:
+// firstName(string), lastName(string), age(number), and location(string)
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 
-const student1: Student = {
-	firstName: "Aggrey",
-	lastName: "Shannon",
-	age: 23,
-	location: "Nairobi"
-};
+// Create two students
+const studentOne: Student = {
+  firstName: 'Olumide',
+  lastName: 'Micheal',
+  age: 23,
+  location: 'Lagos',
+}
 
-const student2: Student = {
-        firstName: "Steve",
-        lastName: "Harver",
-        age: 30,
-        location: "Nairobi"
-};
+const studentTwo: Student = {
+  firstName: 'Tolulope',
+  lastName: 'Micheal',
+  age: 25,
+  location: 'Canada',
+}
 
-const studentsList: Student[] = [student1, student2];
+// create an array named studentsList containing the two variables
+const studentsList: Student[] = [studentOne, studentTwo];
 
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
+// Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
+let table = document.createElement('table');
+let tableBody = document.createElement('tbody');
+table.style.width = '100%';
+table.style.background = '#34ebeb'
 
-table.style.background = "pink";
-table.appendChild(tbody);
-
-studentsList.forEach((student: Student): void => {
-  const row = document.createElement('tr');
-  const nameCell = document.createElement('td');
-  const locationCell = document.createElement('td');
-
-  nameCell.textContent = student.firstName;
-  locationCell.textContent = student.location;
-
-  nameCell.style.border = "1px solid pink";
-  locationCell.style.border = "1px solid pink";
-  nameCell.style.padding = "5px";
-  locationCell.style.padding = "5px";
-
-  row.appendChild(nameCell);
-  row.appendChild(locationCell);
-  tbody.appendChild(row);
+studentsList.forEach(student => {
+  let row = document.createElement('tr');
+  let nameCol = document.createElement('td');
+  let locationCol = document.createElement('td');
+  
+  // Each row should contain the first name of the student and the location
+  nameCol.textContent = student.firstName;
+  locationCol.textContent = student.location;
+  nameCol.style.textAlign = 'center';
+  locationCol.style.textAlign = 'center';
+  nameCol.style.color = '#000';
+  locationCol.style.color = '#000';
+  nameCol.style.border = '1px dotted black';
+  locationCol.style.border = '1px dotted black';
+  nameCol.style.height = '300px';
+  locationCol.style.height = '300px';
+  
+  row.appendChild(nameCol); // append the cell to the row
+  row.appendChild(locationCol); // append the cell to the row
+  tableBody.appendChild(row); // append the row to the tbody
 });
-
-document.body.appendChild(table);
+table.appendChild(tableBody); // append the tbody to the table
+document.body.appendChild(table); // append the table to the body
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,48 +1,45 @@
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    contract: boolean;
-    [propName: string]: any;
-  }
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}. ${lastName}`
+}
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
   
-  interface Directors extends Teacher {
-    numberOfReports: number;
-  }
-  
-  interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-  }
-  
-  const printTeacher: printTeacherFunction = (firstName, lastName) => {
-    return `${firstName[0]}. ${lastName}`;
-  };
-  
-  interface StudentConstructor {
-    new (firstName: string, lastName: string): StudentClassInterface;
-  }
-  
-  interface StudentClassInterface {
-    workOnHomework(): string;
-    displayName(): string;
-  }
-  
-  const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
-    firstName: string;
-    lastName: string;
-  
-    constructor(firstName: string, lastName: string) {
+  constructor(firstName: string, lastName: string){
       this.firstName = firstName;
       this.lastName = lastName;
-    }
-    workOnHomework(): string {
-      return "Currently working";
-    }
-    displayName(): string {
-      return this.firstName;
-    }
-  };
-  
-  export { printTeacher, StudentClass };
+      
+  }
+  workOnHomework() {
+      return 'Currently working';
+  }
+  displayName() {
+      return `${this.firstName}`
+  }
+}
